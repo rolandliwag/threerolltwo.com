2b7f72765dc8f1c1dd411d50e16260957eed5ef6
JR Liwag <liwagr@gmail.com>
2015-11-07 22:31:58 +0400

    Subsequent insert calls with existing email address should not throw error. Add default value to join_date column in email table.

--- server/lib/modules/DAL/EmailAccess.js
+++ server/lib/modules/DAL/EmailAccess.js
@@ -4,13 +4,23 @@
 function EmailAccess(db) {
 	/**
 	 * @param {String} email
-	 * @returns {Promise} 
+	 * @returns {Promise}
 	 */
 	this.add = function (email) {
 		return db.query({
-			text: 'INSERT INTO email VALUES ($1)',
-			values: [email]
-		});
+                text: 'INSERT INTO email VALUES ($1)',
+                values: [email]
+            })
+            .catch(function (err) {
+                return new Promise(function (resolve, reject) {
+                    // If insertion fails due to email already existing, then treat it as a success
+                    if (err.constraint === 'email_pkey' && err.routine === '_bt_check_unique') {
+                        resolve();
+                    } else {
+                        reject(err);
+                    }
+                });
+            });
 	};
 }
 
